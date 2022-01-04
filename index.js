fb552514949021911abb3e49e25946cfb4d9cf61
Zubair Sidhu <sidhuzubair@gmail.com>
2022-01-03 23:03:09 -0600

    Minor adjustments for CTCs usecase (#1)
    * Add console log for debugging
    
    * Change how heroku api secret is accessed
    
    * Revert to process.env
    
    * Add print for testing env var
    
    * Revert changes
    
    * Update comment body, change to reviewComment
    
    * Test getting pr by commit sha
    
    * Get sha from correct place
    
    * Explicitly set github token
    
    * Switch from REST to GraphQL API to get PR number
    
    * Switch to octokit graphql client
    
    * Revert to actions toolkit graphql client
    
    * Add auth headers to query
    
    * Switch back to rest
    
    * Switch to octokit/graphql.js
    
    * Verify deploy status works
    
    * Switch to GITHUB_TOKEN
    
    * Testing graphql output
    
    * Clean up PR number fetch request
    
    * Change github token name
    
    * Correct parameter name
    
    * Change to exit.success when deployment isnt a failure
    
    * Cleanup

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const { Toolkit } = require('actions-toolkit');
+const { graphql } = require('@octokit/graphql');
 const fetch = require('node-fetch');
 
 (async function() {
     const tools = new Toolkit({
         event: ['deployment_status'],
-        secrets: ['GITHUB_TOKEN', 'HEROKU_AUTH_TOKEN']
+        secrets: ['GITHUB_TOKEN', 'HEROKU_AUTH_TOKEN'],
+        token: process.env.GITHUB_TOKEN
     });
 
-    console.log(tools.context);
-
-    // Only continue if this was a failure
+    // Only continue if deployment was a failure
     const deployState = tools.context.payload.deployment_status.state;
     if (deployState !== 'failure') {
-        tools.exit.neutral(`Deploy was not a failure. Got '${deployState}'`);
+        tools.exit.success(`Deploy was not a failure. Got '${deployState}'`);
     }
 
+    const repoOwner = tools.context.payload.repository.owner.login;
     const repoName = tools.context.payload.repository.name;
     const appName = tools.context.payload.deployment.environment;
-    const issueNumber = appName.replace(`${repoName}-pr-`, '');
+
+    // Fetch number of PR associated with this deployment
+    const prNum = await fetchPRNumber(
+      repoOwner,
+      repoName,
+      tools.context.sha
+    );
 
     // Fetch the latest build
     let build = await loadHerokuBuild(appName);
@@ -26,30 +33,61 @@ const fetch = require('node-fetch');
     let logResponse = await fetch(build.output_stream_url);
     const logText = await logResponse.text();
 
-    // Create a comment on the issue with the logs
-    let herokuLink = '<a href="https://dashboard.heroku.com/apps/'+appName+'/activity/">View on Heroku</a>';
+    // Leave comment with build error message on PR
+    const msgBody = "### ⚠️ **Heroku Deployment Failed** ⚠️ \n" + "```\n" + logText + "\n```"
 
-    const issueDetails = {
-        owner: tools.context.payload.repository.owner.login,
-        repo: repoName,
-        issue_number: issueNumber,
-        body: herokuLink+"\n ```\n" + logText + "\n```"
+    const reviewCommentDetails = {
+      owner: repoOwner,
+      repo: repoName,
+      issue_number: prNum,
+      body: msgBody
     };
 
-    await tools.github.issues.createComment(issueDetails)
+    await tools.github.issues.createComment(reviewCommentDetails);
 
-    tools.exit.success("Logs posted");
+    tools.exit.success('Logs posted');
 })();
+
+async function fetchPRNumber(owner, repoName, commitSha) {
+  const prQuery = await graphql({
+    query: `query ($owner: String!, $repoName: String!, $commitSha: GitObjectID!) {
+      repository(owner: $owner, name: $repoName) {
+          object(oid: $commitSha) {
+            ... on Commit {
+              associatedPullRequests (last: 1) {
+                edges {
+                  node {
+                    number
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    owner,
+    repoName,
+    commitSha,
+    headers: {
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const prNum = prQuery.repository.object.associatedPullRequests.edges[0].node.number;
+
+  return prNum;
+}
 
 async function loadHerokuBuild(repoName) {
     const resp = await fetch(`https://api.heroku.com/apps/${repoName}/builds`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${process.env.HEROKU_AUTH_TOKEN}`,
-            Accept: 'application/vnd.heroku+json; version=3',
-            "Content-Type": "application/json; charset=UTF-8",
-            Range: 'created_at; order=desc, max=1;'
-        }
+      method: 'GET',
+      headers: {
+          Authorization: `Bearer ${process.env.HEROKU_AUTH_TOKEN}`,
+          Accept: 'application/vnd.heroku+json; version=3',
+          'Content-Type': "application/json; charset=UTF-8",
+          Range: 'created_at; order=desc, max=1;'
+      }
     });
 
     const firstBuild = (await resp.json())[0];
